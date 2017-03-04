05f94ed1b9ada0ece2ec80dd8f304376f0162529
footzi <footzi@mail.ru>
2017-03-04 09:10:49 +0300

    no message

--- src/js/lessonsInfo.jsx
+++ src/js/lessonsInfo.jsx
@@ -31,34 +31,29 @@ let LessonsInfo = React.createClass({
 		return (
 			<div id="lessons-b-popup">
                 <div className="lessons-b-popup-content">
-                    <div id="b-popup-close-button" onClick={close}>
+                    <div id="b-popup-lesson-close-button" onClick={close}>
                         <Link to="/lessons"><img src="/./images/catalog/close.png" /> </Link>
                     </div>
                     <div className="lesson-order">
-                        <div className="order-title">ЗАКАЗАТЬ / УТОЧНИТЬ</div>
+                        <div className="lesson-title">ЗАПИСАТЬСЯ</div>
                     </div>
-                    <div className="order-image">
+                    <div className="lesson-image">
                             <img src={lesson.image} />
                     </div>
                     <form id="lessonsform" method="post">
                             <div className="lesson-form">
-                           
                                 <input className="lesson-form-input" name="product" type="text" readonly placeholder="Название мастер-класса" value={lesson.name}/>
                                 <input className="lesson-form-input" name="name" type="text" required placeholder="Ваше имя:" />
                                 <input className="lesson-form-input" name="email" type="email" required placeholder="Ваш e-mail:" />
-
-                             
-                                	<select className="lesson-form-input lesson-form-input-data" required name="data">
-                                	<option  value="" disable selected hidden>Дата мастер класса:</option>
-                                	<option className="lesson-form-option">10.05.1990</option>
-                                	<option>10.05.1990</option>
-                                	<option>10.05.1990</option>
-                                	</select>
-                                
-                                
+                                <select className="lesson-form-input lesson-form-input-date" required name="data">
+                                    <option  value="" disable selected hidden>Дата мастер класса:</option>
+                                    <option className="lesson-form-option">10.05.1990</option>
+                                    <option>11.05.1990</option>
+                                    <option>12.05.1990</option>
+                                </select>
                                 <textarea className="lesson-form-input lesson-form-input-message" name="info" type="text" placeholder="Сообщение:" />
                             </div>
-                             <button id="lessonsform-button" className="product-info-button product-order-button">Отправить</button>
+                             <button id="lessonsform-button" className="product-info-button product-lesson-button">Отправить</button>
                     </form>
 
                 </div>            
